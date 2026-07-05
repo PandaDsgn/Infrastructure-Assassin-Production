c38599428abeb50f4c8cb2516b19800418b8f2a3
Vedanta Bandyopadhyay <142706358+PandaDsgn@users.noreply.github.com>
2026-07-05 13:39:21 +0530

    gh pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Infrastructure-Assassin-Prod/",
   server: {
     proxy: {
       "/api": {
